64126199e1d09269d78d2370d4fe3099f47f0ca2
Tavo Cruz <madecruz2@gmail.com>
2023-12-21 12:24:30 -0800

    reset password route

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ const validateMongoDBId = require('../utils/validateMongodbid');
 const { generateRefreshToken } = require('../config/refreshtoken');
 const jwt = require('jsonwebtoken');
 const sendEmail = require('./email');
+const crypto = require('crypto');
 
 // create a user
 const register = asyncHandler(async (req, res) => {
@@ -229,6 +230,25 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
   }
 })
 
+const resetPassword = asyncHandler(async (req, res) => {
+  const { password } = req.body;
+  const { token } = req.params;
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+  if (!user) {
+    throw new Error("Token expired, please try again later");
+  }
+  user.password = password;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save();
+  res.json(user);
+})
+
 module.exports = {
   register,
   login,
@@ -241,5 +261,6 @@ module.exports = {
   handleRefreshToken,
   logout,
   updatePassword,
-  forgotPasswordToken
+  forgotPasswordToken,
+  resetPassword
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express();
-const { register, login, getAllUsers, getUser, deleteUser, updateUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken } = require('../controllers/users.js');
+const { register, login, getAllUsers, getUser, deleteUser, updateUser, blockUser, unblockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword } = require('../controllers/users.js');
 const {authMiddleware, isAdmin}  = require('../middleware/authMiddleware.js');
 
 
@@ -11,6 +11,7 @@ router.route('/all-users').get(getAllUsers);
 router.route('/refresh').get(handleRefreshToken);
 router.route('/update-password').patch(authMiddleware, updatePassword);
 router.route('/forgot-password-token').post(forgotPasswordToken);
+router.route('/reset-password/:token').patch(resetPassword);
 
 router.route('/:id').get(authMiddleware, isAdmin, getUser).delete(deleteUser)
 router.route('/edit-user').patch(authMiddleware, updateUser);
